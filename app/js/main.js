e4b1db63f7047a758a0b176e6a146d66902329a0
PavloPopovV <pavlopopov570@gmail.com>
2024-01-23 02:09:01 +0200

    change

--- app/js/main.js
+++ app/js/main.js
@@ -18,12 +18,49 @@ function clickHandler(e) {
 function openCloseDatepickerPopup(e, method) {
   e.target.nextElementSibling.classList[method]("open");
 }
+//------------------------------- Tabs for Passenger--------------------------
+
+const tabBtns = document.querySelectorAll('.tab-btn');
+const tabsItems = document.querySelectorAll('.tab-content');
+const tabMediaBtns = document.querySelectorAll('.tab-media-btn');
+const tabsMediaItems = document.querySelectorAll('.tab-media-content');
+
+function removeClassActive(btn) {
+  btn.forEach(btnItem => {
+    btnItem.classList.remove('active');
+  });
+}
+
+function removeClassShow(content) {
+  content.forEach(contentItem => {
+    contentItem.classList.remove('show');
+  });
+}
+
+function onTabClick(item, btn, content) {
+  item.addEventListener('click', () => {
+    removeClassActive(btn);
+    removeClassShow(content);
+    item.classList.add('active');
+    let currentBtn = item;
+    let tabId = currentBtn.getAttribute('data-tab');
+    content.forEach(contentItem => {
+      if (tabId === contentItem.getAttribute('id')) {
+        contentItem.classList.add('show');
+      }
+    });
+  });
+}
+
+tabBtns.forEach(item => onTabClick(item, tabBtns, tabsItems));
+tabMediaBtns.forEach(item => onTabClick(item, tabMediaBtns, tabsMediaItems));
 
 document.addEventListener("click", (e) => {
   if (e.target.classList.contains("date-btn")) {
     openCloseDatepickerPopup(e, "add");
   }
 });
+
 document.addEventListener("DOMContentLoaded", function () {
   setValueOnOdometer();
 });
